29d0ab9e343db847ccf2c3f53e70ed0b3bb7a17a
venusblue1708 <venusblue1708@gmail.com>
2023-06-01 18:45:03 +0600

    feat: created variables for inputs and profile in main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 const API = "http://localhost:8000/Post";
+
+//? переменные для профиля
+let nickName = document.querySelector(".nickName");
+let imgProf = document.querySelector(".imgProf");
+let addPost = document.querySelector(".add_post");
+let modal = document.querySelector(".field");
+let follow = document.querySelector(".follow");
+
+//? переменные для инпутов
+let region = document.querySelector("#region");
+let imageUrl = document.querySelector("#image_url");
+let countLike = document.querySelector("#likes");
+let comment = document.querySelector("#comments");
+let btnSend = document.querySelector(".btn_send");
