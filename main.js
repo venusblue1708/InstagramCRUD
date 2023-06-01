c787f3045d22c599dbbd37924105903c849674e6
venusblue1708 <venusblue1708@gmail.com>
2023-06-01 18:50:23 +0600

    feat: function to store data in input

--- main.js
+++ main.js
@@ -13,3 +13,33 @@ let imageUrl = document.querySelector("#image_url");
 let countLike = document.querySelector("#likes");
 let comment = document.querySelector("#comments");
 let btnSend = document.querySelector(".btn_send");
+
+btnSend.addEventListener("click", async function () {
+  let post = {
+    region: region.value,
+    imageUrl: imageUrl.value,
+    countLike: countLike.value,
+    comment: comment.value,
+  };
+  if (
+    !post.region.trim() ||
+    !post.imageUrl.trim() ||
+    !post.countLike.trim() ||
+    !post.comment.trim()
+  ) {
+    alert("complete");
+    return;
+  }
+  await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify(post),
+  });
+  region.value = "";
+  imageUrl.value = "";
+  countLike.value = "";
+  comment.value = "";
+  render();
+});
